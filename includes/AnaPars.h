f65d760afe2e731b1c588b0f420d77c217b8ac61
raeubaen <gargiulo.1824800@studenti.uniroma1.it>
2022-04-15 14:29:21 +0200

    15/04/2022_14:29:21

---
+++ includes/AnaPars.h
@@ -0,0 +1,53 @@
+#ifndef AnaPars_H
+#define AnaPars_H
+
+
+    const int centerMode = 0;
+    const int strontiumMode = 0;
+
+    const Long64_t maxEvToProcess3p1 = 1e3;
+    const Long64_t maxEvToProcess3p2 = 1e3;
+    const Long64_t maxEvToProcess3 = 1e3;
+
+    const TString lutPrefix3p = "../../data/calibration/luts_s3p/";
+    const TString lutPrefix3 = "../../data/calibration/luts_s3/";
+    const TString lutChEqName = "_chargEq";
+    const TString lutChEqErrName = "_chargEqErr";
+    const TString lutBarLenName = "_barLen";
+    const TString lutTimeOffsName = "_timeOff";
+    const TString lutPeakTimeOffsName = "_peakTimeOff";
+    const TString lutZetaOffName = "_zetaOff";
+    const TString lutTimeDiffName = "_timeDiff";
+    const TString lutTimeDiffErrName = "_timeDiffErr";
+
+    const TString cutGPrefix = "../../data/calibration/cutg/";
+    const TString cutGFormat = "cut%i%i";
+    const int enableCutG = 0;
+
+    const int enableOfflineEq = 1;
+
+    const int sideNum = 2;
+    const int scintNum = 8;
+
+    const float scintVp = 12.5;
+    const float scintL = 160.0;
+
+    const double minQCut = 50;
+
+    const double maxQCut = 100000;
+    const double maxVpeak = 1800;
+    const double maxChi2 = 200000;
+    const double maxQSharing = 40;
+    const double chEqReference = 450;
+
+    const float qFrom = 0;
+    const float qTo = 1000; //deve essere 1000
+    const int qBins = 1000; // deve essere 2000
+
+
+
+
+    
+
+    
+#endif
---
+++ includes/Analysis.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#ifndef Analysis_h
+#define Analysis_h
+
+#define ROOT_CLASS ana
+#include "ana.h"
+#include "ana.C"
+
+#define TREE_NAME "CRT"
+
+
+
+class Analysis: public ROOT_CLASS
+{
+public:
+  Analysis(TString, TFile*, TString, TString, TString);
+  virtual void Loop() override; //if not needed, fill with ROOT_CLASS::Loop();
+  void LoopOverEntries();
+  void ProcessHistos();
+  void ProcessPlots();
+  TFile *outFile;
+  TString inFileName;
+  TString runName;
+  TString calName;
+  TString modulSel;
+  TTree *inTree;
+  Int_t iMod[16]; //////sarà poi da 32
+  TBranch *b_iMod; 
+
+  TTree * GetTree(TString infileName) {
+  TTree *tree;
+  TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject(infileName.Data());
+      if (!f || !f->IsOpen()) {
+         f = new TFile(infileName.Data());
+      }
+      f->GetObject(TREE_NAME, tree);
+  inTree = tree;
+  return tree;
+  }
+
+
+};
+
+#endif
+
+Analysis::Analysis(TString infileName, TFile *fileout, TString runN, TString calN, TString modulSelect) : ROOT_CLASS(GetTree(infileName)) { 
+
+  outFile = fileout;
+  inFileName = infileName;
+  runName = runN;
+  calName = calN;
+  modulSel = modulSelect;
+
+  if (modulSel != "") { inTree->SetBranchAddress("iMod", iMod, &b_iMod); } //questo può anche stare nella tree class e darà unknown branch se non lo trova
+
+};
+
+
+
