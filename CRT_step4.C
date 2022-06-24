24bdb01a659345e87d0a3d4627f09bfd7546a628
D-Paesani <58980766+D-Paesani@users.noreply.github.com>
2022-06-24 12:58:21 +0200

    Update CRT_step4.C

--- CRT_step4.C
+++ CRT_step4.C
@@ -1,3 +1,5 @@
+// aggiornato 25 giugno --> data una pulita
+
 #include "includes/ana3.h"
 #include "includes/ana3.C"
 
@@ -11,7 +13,7 @@
 #define inTreeName "CRT"
 #define outTreeName "CRT"
 
-#define crtModulesDistance 30
+#define crtModulesDistance 88.0
 #define crtOffsetBottomX -0.0
 
 using namespace std;
@@ -21,11 +23,6 @@ CsvHandler CSV;
 void CRT_step4(TString runName) {
 
   if (runName == "") {cout<<"runName is empty!!"<<endl; return;}
-
-  double **zetaCalT = CSV.InitMatrix(1, scintNum); 
-  double **zetaCalB = CSV.InitMatrix(1, scintNum); 
-  CSV.Read(CSV.GetFirstFile(lutPrefix3p + runName + "_T" + lutChEqName + "*"),      ',', zetaCalT, 1, scintNum);
-  CSV.Read(CSV.GetFirstFile(lutPrefix3p + runName + "_B" + lutChEqName + "*"),      ',', zetaCalB, 1, scintNum);
 
   TString inFileNameT = Form(inFile_f, runName.Data(), "T");
   TString inFileNameB = Form(inFile_f, runName.Data(), "B");
@@ -38,7 +35,8 @@ void CRT_step4(TString runName) {
   cout<<endl<<"----> runName : "<<runName<<endl;
   cout<<"----> Input files : "<<inFileNameT<<" , "<<inFileNameB<<endl;
   cout<<"----> Output file : "<<outFileName<<endl;
-  cout<<"----> Entries : "<<maxEvB<<" + "<<maxEvT<<endl;
+  cout<<"----> Entries (btm + top) : "<<maxEvB<<" + "<<maxEvT<<endl;
+
 
   TFile *outFile = new TFile(outFileName, "RECREATE");
   outFile->cd();
@@ -48,28 +46,24 @@ void CRT_step4(TString runName) {
   int iSc_out[2];
   double Z_out[2], Q_out[4], T_out[4], X_out[2];
   CRTs4->Branch("iTrig", &iTrig_out, "iTrig/L");
-  CRTs4->Branch("iScint", &iSc_out, "iScint[2]/I");
+  CRTs4->Branch("iScint", &iSc_out, "iScint[2]/I"); 
   CRTs4->Branch("Z", &Z_out, "Z[2]/D");
   CRTs4->Branch("X", &X_out, "X[2]/D");
   CRTs4->Branch("Q", &Q_out, "Q[4]/D");
   CRTs4->Branch("T", &T_out, "T[4]/D");
 
-  HM.SetOutFile(outFile);
-  HM.SetUseFolders(0);
-  HM.AddHistBox("th1f", 1, "thetaR", "thetaR", "", "", 500, -pi, pi);
-  HM.AddHistBox("th1f", 1, "thetaZYvert", "thetaZY", "", "", 500, -1.5, 1.5); 
-  HM.AddHistBox("th1f", 1, "thetaZY", "thetaZY", "", "", 500, -1.5, 1.5);
-  HM.AddHistBox("th1f", 1, "thetaXY", "thetaXY", "", "", 500, -pi/2, pi/2);
-  HM.AddHistBox("th2f", 1, "heatMap", "heatMap", "Z", "cm", "iScint + iMod*(1 + scintNo)", "", 320, -scintL, scintL, 2*scintNum + 2, - 1, 2*scintNum + 1 );
-  HM.AddHistBox("th1f", 1, "mtDiff", "mtDiff", "T", "ns", 1200, -60, 60);
-  HM.AddHistBox("th1f", 1, "mtTop", "mtT", "T", "ns", 1200, -60, 60);
-  HM.AddHistBox("th1f", 1, "mtBtm", "mtB", "T", "ns", 1200, -60, 60);
-  HM.AddHistBox("th1f", 1, "mintDiffCorr", "mintDiffCorr", "T", "ns", 1200, -60, 60);
-  HM.AddHistBox("th2f", 1, "zetaTop_Btm", "Z", "Zbtm", "cm", "Ztop", "cm", 640, -scintL, scintL, 640, -scintL, scintL);
-  HM.AddHistBox("th2f", 1, "xTop_Btm", "x", "xbtm", "cm", "xtop", "cm", scintNum, 0, scintNum, scintNum, 0, scintNum);
-  HM.SetUseFolders(1);
-  HM.AddHistBox("th1f", 4, "zetaTopPerThetaXY", "Z", "Z", "cm", 640, -scintL, scintL);
-  HM.AddHistBox("th1f", 2*scintNum, "zetaPerScint", "Z", "Z", "cm", 640, -scintL, scintL);
+  //plots
+    HM.SetOutFile(outFile);
+    HM.SetUseFolders(0);
+    HM.AddHistBox("th1f", 1, "xTop", "xTop", "X", "cm", 8, -10, 10);
+    HM.AddHistBox("th1f", 1, "xBtm", "xBtm", "X", "cm", 8, -10, 10);
+    HM.AddHistBox("th1f", 1, "mtDiff", "mtDiff", "T", "ns", 1200, -60, 60);
+    HM.AddHistBox("th1f", 1, "thetaZY", "thetaZY", "", "", 500, -1.5, 1.5);
+    HM.AddHistBox("th1f", 1, "thetaXY", "thetaXY", "", "", 500, -pi/2, pi/2);
+    HM.AddHistBox("th2f", 1, "heatMap", "heatMap", "Z", "cm", "iScint + iMod*(1 + scintNo)", "", 320, -scintL, scintL, 2*scintNum + 2, - 1, 2*scintNum + 1 );
+    HM.SetUseFolders(1);
+    HM.AddHistBox("th1f", 2*scintNum, "zetaPerScint", "Z", "Z", "cm", 640, -scintL, scintL);
+  //plots
 
   ana3 anaT(inTreeT), anaB(inTreeB);
 
@@ -92,10 +86,9 @@ void CRT_step4(TString runName) {
     // anaB.fChain->GetEntryWithIndex(iTrig_out);
     // jT++;
 
-    double xT = 2.5*((double)anaT.iSc - 0.5*((double)scintNum - 1)); //mettere in pars --> scintW = 2.5 se non c'è già
-    double xB = 2.5*((double)anaB.iSc - 0.5*((double)scintNum - 1)) + crtOffsetBottomX;
-    //double zT = anaT.pZ, zB = anaB.pZ; // sono sistemate le pseudoZeta????
-    double zT = anaT.Z, zB = anaB.Z; ////pZ
+    double xT = scintW*((double)anaT.iSc - 0.5*((double)scintNum - 1)); //mettere in pars --> scintW = 2.5 se non c'è già
+    double xB = scintW*((double)anaB.iSc - 0.5*((double)scintNum - 1));
+    double zT = anaT.Z, zB = anaB.Z; 
     double mtT{0}, mtB{0};
 
     iSc_out[0] = anaT.iSc; iSc_out[1] = anaB.iSc;
@@ -110,55 +103,33 @@ void CRT_step4(TString runName) {
       mtB += 0.5 * anaB.T[i];
     }
 
-    HM.Fill1d("mtDiff", 0, mtB - mtT);
-    HM.Fill1d("mtTop", 0, mtT);
-    HM.Fill1d("mtBtm", 0, mtB);
-
-    double maxchi2 = max( max(anaB.X2[1], anaB.X2[0]), max(anaT.X2[1], anaT.X2[0]) );
-
-    if (TMath::Abs(mtT-mtB) > 7) {continue;}
-
-    xT += gRandom->Uniform(-1.25, 1.25);
-    xB += gRandom->Uniform(-1.25, 1.25);
-
-    double thetaR = TMath::ATan( (xT - xB) / (zT - zB) );
-    double thetaXY = TMath::ATan( (xT - xB) / crtModulesDistance );
-    double thetaZY = TMath::ATan( (zT - zB) / crtModulesDistance );
-
-    if ( iSc_out[0] == iSc_out[1] )  {HM.Fill1d("thetaZYvert", 0, thetaZY);}
-
-    HM.Fill1d("thetaZY", 0, thetaZY);
-    HM.Fill1d("thetaXY", 0, thetaXY);
-    HM.Fill1d("thetaR", 0, thetaR);
-
-    HM.Fill2d("heatMap", 0, zT, anaT.iSc);
-    HM.Fill2d("heatMap", 0, zB, anaB.iSc + 1 + scintNum);
-
-    HM.Fill1d("zetaPerScint", (double)iSc_out[0], zT);
-    HM.Fill1d("zetaPerScint", (double)iSc_out[1] + scintNum, zB);
-
-    HM.Fill2d("zetaTop_Btm", 0, zT, zB);
-    HM.Fill2d("xTop_Btm", 0, (double)iSc_out[0], (double)iSc_out[1]);
-
-    double tt = TMath::Abs(thetaXY);
-    int idx = (tt > 0.03) + (tt > 0.07) + (tt > 0.12); 
-
-    HM.Fill1d("zetaTopPerThetaXY", idx, zT);
-
-    
-
-
-
-
-
-    
-
-
-
-
-
+    // double maxchi2 = max( max(anaB.X2[1], anaB.X2[0]), max(anaT.X2[1], anaT.X2[0]) );
+    // // if (TMath::Abs(mtT-mtB) > 7) {continue;}
+    // if (maxchi2 > 20) {continue;}
 
     CRTs4->Fill();
+
+    //plot
+
+      double thetaXY = TMath::ATan( (xT - xB - crtOffsetBottomX) / crtModulesDistance ); 
+      double thetaZY = TMath::ATan( (zT - zB) / crtModulesDistance );
+
+      HM.Fill1d("thetaZY", 0, thetaZY);
+      HM.Fill1d("thetaXY", 0, thetaXY);
+
+      HM.Fill2d("heatMap", 0, zT, anaT.iSc);
+      HM.Fill2d("heatMap", 0, zB, anaB.iSc + 1 + scintNum);
+
+      HM.Fill1d("zetaPerScint", (double)iSc_out[0], zT);
+      HM.Fill1d("zetaPerScint", (double)iSc_out[1] + scintNum, zB);
+
+      HM.Fill1d("mtDiff", 0, mtB - mtT);
+      
+      HM.Fill1d("xTop", 0, xT);
+      HM.Fill1d("xBtm", 0, xB);
+
+
+    //plot
 
   }
   
